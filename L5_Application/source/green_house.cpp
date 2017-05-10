881177b5837949f513e3d7d5a51c7d70b94d9158
Abhishek Singh <singh.abhishek21@gmail.com>
2017-05-10 00:35:06 -0700

    LCD: display look and feel changed

--- L5_Application/source/green_house.cpp
+++ L5_Application/source/green_house.cpp
@@ -17,9 +17,16 @@
 
 
 Adafruit_RA8875 tft(3,4);
-char lsReading[100];
-char tempReading[100];
-char humidityReading[100];
+char lsReading[30];
+char tempReading[30];
+char humidityReading[30];
+char sprinkler[20];
+char bulb[20];
+char fan[20];
+
+bool sprinkler_status;
+bool bulb_status;
+bool fan_status;
 
 struct senReadings_t
 {
@@ -50,7 +57,7 @@ void sensor_readings(void * p)
 		readings.humidity_sensor = readH();
 		u0_dbg_printf("%d - %d - %f - %f\n", readings.light_sensor_raw, readings.light_sensor_percentage,
 											 readings.temperature_sensor, readings.humidity_sensor);
-		delay_ms(100);
+		vTaskDelay(100);
 	}
 }
 
@@ -63,25 +70,39 @@ bool display_image()
 	}
 	tft.graphicsMode();
 	tft.setLayer(L1);
-	tft.fillScreen(RA8875_RED);
-	tft.setLayer(L2);
 	tft.textMode();
-    tft.textColor(RA8875_BLUE,RA8875_WHITE);
 	tft.textEnlarge(2);
 	return true;
 }
 
 void display_reading()
 {
-	snprintf(lsReading, 100, "Light Reading = %d", readings.light_sensor_percentage);
-	tft.textSetCursor(100, 40);
+	tft.fillScreen(RA8875_BLUE);
+    tft.textColor(RA8875_WHITE, RA8875_BLACK);
+	snprintf(lsReading, 30, "Light Reading = %d", readings.light_sensor_percentage);
+	tft.textSetCursor(190, 40);
 	tft.textWrite(lsReading);
-	snprintf(tempReading, 100, "Temperature Reading = %.2f C", readings.temperature_sensor);
-	tft.textSetCursor(100, 150);
+	snprintf(tempReading, 30, "Temperature Reading = %.2f C", readings.temperature_sensor);
+	tft.textSetCursor(50, 150);
 	tft.textWrite(tempReading);
-	snprintf(humidityReading, 100, "Humidity Reading = %.2f", readings.humidity_sensor);
-	tft.textSetCursor(100, 260);
+	snprintf(humidityReading, 30, "Humidity Reading = %.2f", readings.humidity_sensor);
+	tft.textSetCursor(120, 250);
 	tft.textWrite(humidityReading);
+	tft.layerEffect(OR);
+	snprintf(sprinkler, 20, "Sprinkler=%s", sprinkler_status?" ON":"OFF");
+	sprinkler_status?tft.textColor(RA8875_WHITE, RA8875_RED):tft.textColor(RA8875_WHITE, RA8875_BLACK);
+	tft.textSetCursor(10, 370);
+	tft.textWrite(sprinkler);
+	tft.layerEffect(OR);
+	snprintf(bulb, 20, "Bulb=%s", bulb_status?" ON":"OFF");
+	bulb_status?tft.textColor(RA8875_WHITE, RA8875_RED):tft.textColor(RA8875_WHITE, RA8875_BLACK);
+	tft.textSetCursor(360, 370);
+	tft.textWrite(bulb);
+	tft.layerEffect(OR);
+	snprintf(fan, 20, "Fan=%s", fan_status?" ON":"OFF");
+	fan_status?tft.textColor(RA8875_WHITE, RA8875_RED):tft.textColor(RA8875_WHITE, RA8875_BLACK);
+	tft.textSetCursor(605, 370);
+	tft.textWrite(fan);
 	tft.layerEffect(OR);
 }
 
@@ -90,28 +111,34 @@ void actuator_action()
 	if(readings.temperature_sensor > 30)
 	{
 		LE.on(1); // Represents Fan: ON
+		fan_status = true;
 	}
 	else
 	{
 		LE.off(1); // // Represents Fan: OFF
+		fan_status = false;
 	}
 
 	if(readings.humidity_sensor < 40)
 	{
 		LE.on(2); // Represents Sprinkler: ON
+		sprinkler_status = true;
 	}
 	else
 	{
 		LE.off(2); // // Represents Sprinkler: OFF
+		sprinkler_status = false;
 	}
 
 	if(readings.light_sensor_percentage < 50)
 	{
 		LE.on(3); // Represents Bulb: ON
+		bulb_status = true;
 	}
 	else
 	{
 		LE.off(3); // Represents Bulb: OFF
+		bulb_status = false;
 	}
 
 }
@@ -124,6 +151,6 @@ void display_and_actuate(void *p)
 		actuator_action();
 		if(lcd_status == true)
 			display_reading();
-		delay_ms(1000);
+		vTaskDelay(500);
 	}
 }
